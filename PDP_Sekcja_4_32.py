5d13f42ef97ce36b49086a8a3c6088d517ff8f30
AdamWsk <adam.wosiek@gmail.com>
2025-04-15 19:36:10 +0200

    Create PDP_Sekcja_4_32.py

---
+++ PDP_Sekcja_4_32.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 15 19:33:42 2025
+
+@author: VIK
+"""
+
+print('TVP1','TVP2','TVN','Polsat','BBC','HBO','MTV')
+print('TVP1','TVP2','TVN','Polsat','BBC','HBO','MTV', sep=';')
+print('I like computers ','TVP1','TVP2','TVN','Polsat','BBC','HBO','MTV', sep=' but better is ')
+
+
+ProgramName = 'BBC'
+Item = 'News'
+Time = '18:00'
+print('I like watching',Item,'at',Time,'on',ProgramName,'.')
+print('I like watching ',Item,' at ',Time,' on ',ProgramName,'.', sep='')
