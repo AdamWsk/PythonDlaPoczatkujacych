55dc56602bb99aba2d6c869496759687f98a7849
AdamWsk <adam.wosiek@gmail.com>
2025-04-16 13:53:46 +0200

    Create PDP_Sekcja_4_38.py

---
+++ PDP_Sekcja_4_38.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 13:38:10 2025
+
+@author: VIK
+"""
+
+#Zadanie 1
+firstName = 'Kasia'
+familyName = 'Sowa'
+lastName = 'Mrugala'
+
+newName = firstName + ' ' + familyName + ' ' + lastName
+print(newName)
+
+#Zadanie 2 i 3
+music = '"Universal Fanfare" Jerry Goldsmith\n"Happy Together" Garry Bonner\n"I\'m a Man" Steve Winwood '
+print(music)
+
+#Zadanie 4
+print(' (\\(\\ \n ( -.-)\n O_(")(")')
+
+#or
+
+print('(\\(\\') 
+print('(-.-)') 
+print('O_(")(")')
