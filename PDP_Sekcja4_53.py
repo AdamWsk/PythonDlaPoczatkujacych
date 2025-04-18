a224bb31c9ee695f6191d1c6a2203da49cd36b92
AdamWsk <adam.wosiek@gmail.com>
2025-04-18 23:39:05 +0200

    Lab 53 + 50

---
+++ PDP_Sekcja4_53.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 18 23:06:23 2025
+
+@author: VIK
+"""
+
+v1 = 126
+
+v2 = '126'
+
+v3 = 126.0
+
+v4 = '126.0'
+
+print('v1 is', type(v1))
+print('v2 is', type(v2))
+print('v3 is', type(v3))
+print('v4 is', type(v4))
+
+sum1 = v1 + v3
+print('sum1 =', sum1,'and tpe is', type(sum1))
+
+sum2 = v2 + v4
+print('sum2 =', sum2,'and tpe is', type(sum2))
+
+print(7-1*0+3+3)
+
+# Ile to jest  4 do potęgi piątej podzielone przez 2 do potęgi 3
+
+print(4**5/2**3)
+
+'''
+7. To zadanie to raczej matematyczna łamigłówka, niż typowe zadanie z Pythona, ale z drugiej strony to "smaczek" bycia programistą lub matematykiem...
+
+Wyobraż sobie, że zepsuła Ci się klawiatura. Działa tylko klawisz 9, klawisze z działaniami +-*/ oraz klawisze nawiasów i enter. Na dodatek klawisz 9 możesz nacisnąć tylko maksymalnie 4 razy, bo po kolejnym naciśnięciu.... komputer się restartuje.
+
+Twoim zadaniem jest napisanie na tej zepsutej klawiaturze w interpreterze Python takiego działania, aby jego wynik wynosił 100
+'''
+
+print(99+9/9)
