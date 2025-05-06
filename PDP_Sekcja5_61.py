1ba19e6d2d57a1878fe3ff3d0d5d2afefeb90ff3
AdamWsk <adam.wosiek@gmail.com>
2025-05-06 15:55:32 +0200

    Sekcja 5 / 61

---
+++ PDP_Sekcja5_61.py
@@ -0,0 +1,17 @@
+hitsTitles = ['BROTHERS IN ARMS','BOHEMIAN RHAPSODY','STAIRWAY TO HEAVEN','RIDERS ON THE STORM','WISH YOU WERE HERE']
+
+print('Oto tytuly z listy przebojow:', hitsTitles)
+
+hitsTitles.append('CHILD IN TIME')
+hitsTitles.append('AGAIN')
+
+print('Oto tytuly z listy przebojow:', hitsTitles)
+
+hitsTitles.insert(3,'HOTEL CALIFORNIA')
+
+print(hitsTitles)
+
+hitsTitles.insert(0,'THE SOUND OF SILENCE')
+print(hitsTitles)
+
+print(hitsTitles.index('HOTEL CALIFORNIA'))
