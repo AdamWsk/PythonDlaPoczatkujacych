3ceda3be69ff51e152faafe4d3342244b44deaac
AdamWsk <adam.wosiek@gmail.com>
2025-05-22 02:02:52 +0200

    Create PDP_Sekcja_5_67.py

---
+++ PDP_Sekcja_5_67.py
@@ -0,0 +1,19 @@
+
+#1
+chanels = {'Google': 1480, 'Email': 300, 'Natural Traffic': 440, 'TV Spot': 700}
+
+#2
+print(chanels['Email'])
+
+#3
+chanelsUpdate = { 'Natural Traffic' : 520, 'News' : 600}
+
+#4
+chanels.update(chanelsUpdate)
+print(chanels)
+#5
+print(chanels.keys())
+
+#6
+chanels.pop('Email')
+print(chanels)
